bb7698680570757932cff861a2afdb32473dacc7
OpenYNU <hanson4201@outlook.com>
2024-05-05 22:08:46 +0800

    更新 gif.py

--- Project/gif动画/gif.py
+++ Project/gif动画/gif.py
@@ -3,13 +3,13 @@ import imageio
 # 指定图片所在的目录
 image_directory = "./"
 # 输出GIF的名称
-output_gif = "output1.gif"
+output_gif = "output2.gif"
 
 # 创建一个空列表来存储读入的图像
 images = []
 
-# 循环遍历所有的帧，从264到359
-for i in range(264, 288):
+# 循环遍历所有的帧，从264到289
+for i in range(2, 300):
     # 生成每一帧的文件名
     filename = f"../Atmos Physical/data/2008_01_map/{image_directory}frame_{i:04d}.png"
     # 读取图片并添加到列表中
